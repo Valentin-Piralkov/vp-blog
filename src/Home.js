cbf10354fcdbd61ea808a499b4d139c6bd1f9597
Valentin Piralkov <valiopiralkov@gmail.com>
2023-08-29 19:16:04 +0300

    custom hooks

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,14 @@
-import { useEffect, useState } from "react"; //impoert hook
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-            })
-    }, []);
-
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
+    
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={ blogs } title = "All Blogs!" />}
         </div>
     );
