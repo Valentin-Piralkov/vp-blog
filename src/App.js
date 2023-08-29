4eb5d8f6677638a7bf92f6be385c599199b64408
Valentin Piralkov <valiopiralkov@gmail.com>
2023-08-29 20:01:12 +0300

    final

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() { //a component is function
   return ( // we should always return something in that function
@@ -20,6 +21,9 @@ function App() { //a component is function
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
